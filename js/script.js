fddecaae15a984c81af85bfdc272aa76fe92f869
Nicholas Zentai <nicholasz8687@gmail.com>
2023-03-14 14:37:34 -0400

    updated js code to generate numbers and letters in random password

--- js/script.js
+++ js/script.js
@@ -11,24 +11,24 @@ function showPassword(password) {
 }
 
 function generatePassword (optionsObj) {
-    var low = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    var up = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    var numb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     var specChar = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '?', '/', '<', '>', '|', '{', '}', '[', ']', ':', ';', '.', ','];
 
     var passwordChoices = [];
     var passwordList = [];
 
-    if (optionsObj.low) {
-        passwordList = passwordList.concat(low);
+    if (optionsObj.lower) {
+        passwordList = passwordList.concat(lower);
     }
 
-    if (optionsObj.up) {
-        passwordList = passwordList.concat(up);
+    if (optionsObj.upper) {
+        passwordList = passwordList.concat(upper);
     }
 
-    if (optionsObj.numb) {
-        passwordList = passwordList.concat(numb);
+    if (optionsObj.nums) {
+        passwordList = passwordList.concat(nums);
     }
 
     if (optionsObj.specChar) {
