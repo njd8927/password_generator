831e6b90aafb28a446089348499e260e10dfa564
Nicholas Zentai <nicholasz8687@gmail.com>
2023-03-20 21:28:27 -0400

    updated pseudocode

--- js/script.js
+++ js/script.js
@@ -15,10 +15,12 @@ function generatePassword (optionsObj) {
     var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     var specChar = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '?', '/', '<', '>', '|', '{', '}', '[', ']', ':', ';', '.', ','];
-
+// variables created for the password options the user has containing arrays for lowercase, uppercase, numbers and special characters/
+// global variables created for password choices the user inputs and password list for the password to be generated
     var passwordChoices = [];
     var passwordList = [];
-// created if statements so the user chosen criteria is being used in the random password
+// created if statements so the user chosen criteria is being used in the random password.
+// .concat used to combine strings into one
     if (optionsObj.lower) {
         passwordList = passwordList.concat(lower);
     }
@@ -35,6 +37,7 @@ function generatePassword (optionsObj) {
         passwordList = passwordList.concat(specChar);
     }
 //  created for loop for the password length based on what the user entered
+// created variable for a random password using the math object
     for (var i = 0; i < optionsObj.amount; i++) {
         var randomSelection = passwordList[Math.floor(Math.random() * passwordList.length)];
 
@@ -56,7 +59,7 @@ function getOptions(){
     var parsedAmount = parseInt (passwordLength);
 // used parseInt to obtain a number from a string entered by the user
     console.log(parsedAmount);
-
+// created variable with an object showing key value pairs based on user inputs regarding what they want to include in the password
     var options = {
         upper: wantsUppercase,
         lower: wantsLowercase,
